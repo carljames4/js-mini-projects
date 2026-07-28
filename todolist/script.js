58068b8fb8b3b533858cd227179f22e2fbae4ab4
carljames4 <magsipocjames93@gmail.com>
2026-07-28 13:13:35 +0800

    Re-add deleted file

---
+++ todolist/script.js
@@ -0,0 +1,23 @@
+        let list = [];
+
+        function addList() {
+            let value = document.getElementById("input").value;
+
+            list.push(value);
+
+            showList();
+        }
+
+        function showList() {
+            let text = ""
+            for(let i = 0; i < list.length ; i++) {
+                text += `${list[i]} <button onclick="deletelist(${i})">delete</button><br>`;
+
+            }
+                document.getElementById("output").innerHTML = text;
+        }
+
+        function deleteList(index) {
+            list.splice(index, 1);
+            showlist();
+n        }
